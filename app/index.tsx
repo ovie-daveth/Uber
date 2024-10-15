9beaa16d4839c286de28db0c3e24cb3a46d0dda5
Omokefe Ovie David <ovie.omokefe@premiumtrustbank.com>
2024-10-15 18:51:18 +0100

    Setting up

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import  {StatusBar } from "expo-status-bar"
+
+const index = () => {
+  return (
+    <SafeAreaView className='h-full bg-black'>
+        <StatusBar style='light' />
+        <View className='flex-1 items-center justify-center'>
+            <Text className='text-white '>index</Text>
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default index
