b3d822d42dbdb25ec8151dd0af82be274f928c18
ovie-daveth <oviedavid77@gmail.com>
2024-11-03 02:17:52 +0000

    mackdatas

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,16 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, FlatList } from "react-native";
 import React from "react";
 import Icon from 'react-native-vector-icons/Feather';
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
+import { MOCK_RIDERS } from "@/constants/mockdata";
+import RideCard from "@/components/RideCard";
 
 const Home = () => {
     return (
-        <SafeAreaView>
+        <SafeAreaView className="bg-general-500 h-full">
             <StatusBar style="dark" />
-            <View>
-            <Icon name="home" size={30} color="#000"  />
-        </View>
+            <View className="bg-white shadow-md w-12 p-2 rounded-full ml-2 mt-2">
+                <TouchableOpacity activeOpacity={0.2}>
+                <Icon name="menu" size={30} color="#7d7d7d" className=""  />
+                </TouchableOpacity>
+            </View>
+            <FlatList 
+            data={MOCK_RIDERS}
+            renderItem={({item}) => <RideCard data={item} />}
+            />
         </SafeAreaView>
     );
 };
---
+++ components/RideCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const RideCard = ({data}: any) => {
+  return (
+    <View>
+      <Text>RideCard</Text>
+    </View>
+  )
+}
+
+export default RideCard
